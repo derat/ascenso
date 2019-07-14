6b30b961d9d2b27c05c764f6fb3b972bc2c64db1
Niniane Wang <niniane@gmail.com>
2019-07-14 09:04:16 -0700

    use vuetify to create app scaffolding, move Climbs into scaffolding

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-toolbar color="blue">
+      <v-toolbar-title class="white--text">Climbing competition</v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <Climbs/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Climbs from './components/Climbs'
+
+export default {
+  name: 'App',
+  components: {
+    Climbs
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
