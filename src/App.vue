018a409da3f7222660a78b12264daf32302d8d7b
Daniel Erat <dan@erat.org>
2019-07-15 08:48:11 -0400

    add (not-yet-active) navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
   <v-app>
-    <v-toolbar color="blue">
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-tile
+          v-for="item in navItems"
+          v-bind:key="item.name"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-title>{{ item.name }}</v-list-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar
+      color="blue"
+      app
+    >
+      <v-toolbar-side-icon
+        @click.stop="drawer = !drawer"
+        class="white--text"
+      ></v-toolbar-side-icon>
       <v-toolbar-title class="white--text">Climbing competition</v-toolbar-title>
     </v-toolbar>
 
@@ -20,7 +47,18 @@ export default {
   },
   data () {
     return {
-      //
+      drawer: null,
+      navItems: [
+        { name: "Climbs",
+          icon: "view_list",
+        },
+        { name: "Scoreboard",
+          icon: "assessment",
+        },
+        { name: "Statistics",
+          icon: "score",
+        },
+      ],
     }
   }
 }
