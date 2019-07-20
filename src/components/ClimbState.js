23e11005b6937f1df55273092f58af2262b87440
Daniel Erat <dan@erat.org>
2019-07-20 12:17:47 -0400

    Add LICENSE, AUTHORS, and copyright headers. (#16)

--- src/components/ClimbState.js
+++ src/components/ClimbState.js
@@ -1,3 +1,7 @@
+// Copyright 2019 Daniel Erat and Niniane Wang. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 export default Object.freeze({
   NOT_CLIMBED: 0,
   LEAD: 1,
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,3 +1,7 @@
+// Copyright 2019 Daniel Erat and Niniane Wang. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // This file initializes Firebase and exports various objects so they can be
 // used by components.
 //
