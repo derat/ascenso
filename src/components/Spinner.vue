48ec2329b96a4c83132ff2f4c1afd3fab138f0a4
Daniel Erat <dan@erat.org>
2019-07-21 13:56:28 -0400

    Display basic statistics.
    Update the Statistics view to compute and display points and
    climbs. Also create a new Spinner component for displaying a
    fullscreen indeterminate-progress indicator.

---
+++ src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<!-- Copyright 2019 Daniel Erat and Niniane Wang. All rights reserved.
+     Use of this source code is governed by a BSD-style license that can be
+     found in the LICENSE file. -->
+
+<template>
+  <v-container fill-height>
+    <v-layout column justify-center align-center>
+      <v-progress-circular indeterminate size="48" />
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
