881a7bedce1a5147617ea62b27da98bfd636cbb2
Daniel Erat <dan@erat.org>
2019-08-17 19:42:05 -0400

    Add favicons and manifest.json. (#78)
    Add a bunch of favicons that were generated by
    realfavicongenerator.net based on the fallback logo, along
    with a manifest.json file (the first step to making this a
    PWA).
    
    Update vue.config.js to configure copy-webpack-plugin to
    perform rudimentary "{{ VUE_APP_SOME_VAR }}" substitution of
    environment variables when processing browserconfig.xml and
    manifest.json. html-webpack-plugin unfortunately only
    performs substitions in the index.html file.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,38 @@
 module.exports = {
+  chainWebpack: config => {
+    // html-webpack-plugin performs substitution of environment variables in
+    // public/index.html, but doesn't touch any other files in the public dir.
+    // Tap into copy-webpack-plugin (which is used to copy the dir) and set its
+    // 'transform' option to process additional files. See
+    // https://cli.vuejs.org/guide/webpack.html#modifying-options-of-a-plugin
+    // and https://github.com/vuejs/vue-cli/issues/2436 for more details. 'vue
+    // inspect' can be used to see the existing config.
+    config.plugin('copy').tap(args => {
+      const options = args[0][0];
+      // |content| is a Node buffer: https://nodejs.org/api/buffer.html
+      // |path| contains the source file's absolute path.
+      options.transform = (content, path) => {
+        // Replace '{{ VUE_APP_SOME_ENV_VAR }}' with the value of the
+        // corresponding environment variable (defined in .env or .env.local).
+        if (
+          path.endsWith('/browserconfig.xml') ||
+          path.endsWith('/manifest.json')
+        ) {
+          content = Buffer.from(
+            content.toString().replace(/{{\s*(\S+)\s*}}/g, (match, p1) => {
+              const val = process.env[p1];
+              if (val === undefined) {
+                throw new Error(`Environment variable "${p1}" undefined`);
+              }
+              return val;
+            })
+          );
+        }
+        return content;
+      };
+      return args;
+    });
+  },
   devServer: {
     disableHostCheck: true,
   },
